ceb992e59efac0cf4b5d4afc89eaacfcec153ca7
Nuria Toledano <ia.andyouare@gmail.com>
2022-07-25 13:20:01 +0100

    Created basic CRUD actions in ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,49 @@
 class ReviewsController < ApplicationController
+
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
   def index
-    @reviews = [
-      "The Smile",
-      "Five Guys",
-      "St John",
-      "Baby Bo's"
-    ]
+    @reviews = Review.all
+  end
+
+  def show
   end
 
   def new
+    @review = Review.new
+  end
 
+  def create
+    @review = Review.new(review_params)
+    @review.save
+
+    redirect_to reviews_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to @review
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+
+    redirect_to reviews_path
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:restaurant, :title, :body, :score)
   end
 end
